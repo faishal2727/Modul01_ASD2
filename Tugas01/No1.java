c9fafc546a74a7b1ce8237d97bb0794e70d7f07c
Faishal Rizal <72137450+muhfaishalrizal@users.noreply.github.com>
2021-04-01 20:37:16 +0700

    Add files via upload

---
+++ Tugas01/No1.java
@@ -0,0 +1,12 @@
+package Tugas01;
+public class No1 {
+    public static void main(String[] args) {
+        for (int f = 0; f <=4; f++) {
+            for (int r = 0; r<f; r++) {
+                System.out.print(f);
+            }
+            System.out.println();     
+        }
+        System.out.println("Muh.Faishal Rizal");
+    }
+}
---
+++ Tugas01/No2.java
@@ -0,0 +1,10 @@
+package Tugas01;
+public class No2 {
+    public static void main(String[] args) {
+        String [] siswa = {"Reinan","Odena","Geano"};
+        for (int f = 0; f < siswa.length; f++) {
+            System.out.println("Indeks ke - "+f+" = "+siswa[f]);      
+        }
+        System.out.println("Muh.Faishal Rizal");
+    }
+}
